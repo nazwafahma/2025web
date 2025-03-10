3323e255ea40eaa8397f3e92b532f9ba6493fa6d
nazwafahma <nazwafahma3@gmail.com>
2025-03-10 13:06:09 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <h1>DATA MAHASISWA<h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <th>No</th>
+            <th>Nim</th>
+            <th>Nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323044</td>
+                <td>Nazwa</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323048</td>
+                <td>Puteri</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
