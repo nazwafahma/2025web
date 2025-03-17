6e897079e3417b603a318f32535ccc2f8fe3eb11
nazwafahma <nazwafahma3@gmail.com>
2025-03-17 22:07:43 +0800

    READ DATA

---
+++ koneksi.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$database = "web4a";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+function ambildata($query)
+{
+    $conn = $GLOBALS['conn'];
+    $hasil = mysqli_query($conn, $query);
+
+    $data = [];
+    while ($baris = mysqli_fetch_assoc($hasil)) {
+        $data[] = $baris;
+    }
+    return $data;
+}
